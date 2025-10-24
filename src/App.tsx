588811ad62acf77559caf96a03659b114260c1a8
Yuhei Yamada <pepushi514@gmail.com>
2025-10-24 15:19:33 +0900

    AppLayoutの作成

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Report from './pages/Report';
 import NoMatch from './pages/NoMatch';
+import AppLayout from './components/layout/AppLayout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/report" element={<Report />} />
-        <Route path="*" element={<NoMatch />} />
+        <Route path="/" element={<AppLayout/>}>
+          <Route index element={<Home />} />
+          <Route path="/report" element={<Report />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
