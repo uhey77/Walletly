e89a9a5f1ac4bc1e0ce8c8de135e6166d59c05b9
Yuhei Yamada <pepushi514@gmail.com>
2025-10-27 18:28:29 +0900

    フォントのインストール

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,23 @@ import Home from './pages/Home';
 import Report from './pages/Report';
 import NoMatch from './pages/NoMatch';
 import AppLayout from './components/layout/AppLayout';
+import { theme } from './theme/theme';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
   return (
     <BrowserRouter>
+      <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Routes>
         <Route path="/" element={<AppLayout/>}>
           <Route index element={<Home />} />
           <Route path="/report" element={<Report />} />
           <Route path="*" element={<NoMatch />} />
-        </Route>
-      </Routes>
+          </Route>
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
